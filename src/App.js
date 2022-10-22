fe6c73f515ecb140515353196c97b5e30cdd77d8
mule <belmeetmule@gmail.com>
2022-10-22 16:57:54 +0300

    update pages routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Route, Routes } from 'react-router';
 import HomePage from './Pages/HomePage';
-import LeagueDetails from './Components/Leagues/LeagueDetails';
-import About from './Pages/AboutPage';
+import LeaguePage from './Pages/LeaguePage';
+import AboutPage from './Pages/AboutPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/detail/:leaguesId" element={<LeagueDetails />} />
-      <Route path="/About" element={<About />} />
+      <Route path="/League/:leaguesId" element={<LeaguePage />} />
+      <Route path="/About" element={<AboutPage />} />
     </Routes>
   );
 }
